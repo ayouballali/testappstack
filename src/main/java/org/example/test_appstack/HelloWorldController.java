a2ebb0e959b21e2233be51c2324e60aa8904510c
aallali <ayoub.allali@oracle.com>
2024-06-05 16:58:02 +0100

    Changes

--- src/main/java/org/example/test_appstack/HelloWorldController.java
+++ src/main/java/org/example/test_appstack/HelloWorldController.java
@@ -1,2 +1,20 @@
-package org.example.test_appstack;public class HelloWorldController {
+package org.example.test_appstack;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/check")
+    public String f(){
+        return "I'm alive";
+    }
+
+    @GetMapping("/")
+    public String f(){
+        return "I'm alive";
+    }
 }
