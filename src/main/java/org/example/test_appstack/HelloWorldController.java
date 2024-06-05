d24bbd1be0ec345fbea67b3235c1910dff3bb66a
aallali <ayoub.allali@oracle.com>
2024-06-05 10:06:09 +0100

    Initial commit

---
+++ src/main/java/org/example/test_appstack/HelloWorldController.java
@@ -0,0 +1,2 @@
+package org.example.test_appstack;public class HelloWorldController {
+}
---
+++ src/main/java/org/example/test_appstack/TestAppstackApplication.java
@@ -0,0 +1,13 @@
+package org.example.test_appstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAppstackApplication.class, args);
+	}
+
+}
